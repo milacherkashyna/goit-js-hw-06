149e83502e58c87a7ffdba7905b2dad1619c6335
milacherkashyna <mila.cherkashyna@gmail.com>
2023-02-01 18:02:07 +0200

    homework in process

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  if (form.elements.email.value === "" || form.elements.password.value === "") {
+    alert("Введіть ваші дані");
+    return;
+  }
+  console.log({
+    email: form.elements.email.value,
+    password: form.elements.password.value,
+  });
+  form.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector("span");
+const body = document.querySelector("body");
+
+button.addEventListener("click", () => {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+});
